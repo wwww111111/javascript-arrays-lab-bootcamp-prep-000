b09871f71da7cdcf6fd286302ce7d1ec7c7219a0
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 16:08:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.lenght-1);
+  return kittens.slice(0, kittens.length-1);
 }
 
 function removeFirstKitten() {
